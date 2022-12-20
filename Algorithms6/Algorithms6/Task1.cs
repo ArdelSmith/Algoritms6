600be09715d5e9eff03f037d689bfb102d8b27c2
Ivan Danilyuk <werwetsad@gmail.com>
2022-12-20 19:13:14 +0500

    Задание 1
    Создана основа для первого задания

---
+++ Algorithms6/Algorithms6/Task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class Pair
+    {
+        public string key;
+        public List<string> values = new List<string>();
+    }
+    public class Table
+    {
+        Pair[] table;
+        public Table()
+        {
+            table = new Pair[10000];
+        }
+        /// <summary>
+        /// Вставляет элемент в таблицу
+        /// </summary>
+        /// <param name="elem"></param>
+        public void Insert(string elem)
+        {
+
+        }
+        /// <summary>
+        /// Удаляет элемент из таблицы
+        /// </summary>
+        /// <param name="elem"></param>
+        public void Delete(string elem)
+        {
+
+        }
+        /// <summary>
+        /// Находит элемент в таблице
+        /// </summary>
+        /// <param name="elem"></param>
+        public void Find(string elem)
+        {
+
+        }
+        public static string CalculateHash()
+        {
+            return null;
+        }
+    }
+    public static class Task1
+    {
+
+    }
+}
